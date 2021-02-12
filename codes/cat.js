f94c03df23d697e40a9c34119c5bd27d7bf2f620
legend-js <75608223+legend-js-dev@users.noreply.github.com>
2021-02-12 22:11:28 +0545

    Update cat.js

--- codes/cat.js
+++ codes/cat.js
@@ -1,12 +1,12 @@
 const { random } = require("discord-utils.js")
 
 module.exports = {
-name: "communist",
+name: "cat",
 description: "sends an image with the communist filter",
 run: async (client, message, args) => {
 let user = message.mentions.users.first() || message.author
 let avatar = user.displayAvatarURL()
-let image = await random.dog()
+let image = await random.cat()
 message.channel.send({
 files: [{
 name: "image.png",
